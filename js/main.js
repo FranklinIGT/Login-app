e2fc1102c280efdbd37dd3355c227321fe09d885
franklin <franklining2019@gmail.com>
2022-06-18 09:34:44 -0400

    url-redirect

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+let urlLogin='https://franklinigt.github.io/login-app/';
+let urlHome ='https://franklinigt.github.io/login-app/home';
+
 /*login simulation redirect */
 
 let url=window.location.href;
@@ -6,20 +9,20 @@ const redirect=(url)=>{
             const status= JSON.parse(localStorage.getItem('status'));
             console.log(url) 
             if(status.status!='active'){
-                 return location.href = `http://127.0.0.1:5500/`;
+                 return location.href = urlLogin;
                     
             }
  }
- if(url==='http://127.0.0.1:5500/home.html'){
+ if(url=== urlHome){
  redirect(url)
  let user=document.getElementById('User')
  let jsonUser=JSON.parse(localStorage.getItem('user'))
  user.innerHTML=` WELCOME ${jsonUser.username}`;
  } 
- if(url==='http://127.0.0.1:5500/'){
+ if(url===urlLogin){
     const status= JSON.parse(localStorage.getItem('status'));
-    if(status.status==='active' && url===`http://127.0.0.1:5500/`){
-        location.href ='http://127.0.0.1:5500/home.html'
+    if(status.status==='active' && url===urlLogin){
+        location.href =urlHome
     }
 }
 
@@ -40,7 +43,7 @@ for (let i = 0; i < inputs.length; i++) {
 }
 /*Animation user-b*/
 /*local store*/
-if(url==='http://127.0.0.1:5500/'){
+if(url===urlLogin){
     
 let username= document.getElementById('username')
 let password = document.getElementById('password')
@@ -74,13 +77,13 @@ submit.addEventListener('click',(e)=>{
     localStorage.setItem('user',JSON.stringify(user));
     localStorage.setItem('status',JSON.stringify({status:'active'}));
     console.log(user)
-    location.href = 'http://127.0.0.1:5500/home.html';
+    location.href = urlHome;
 }
 
 let logout = document.getElementById('logout');
 logout.addEventListener('click',(e)=>{
     e.preventDefault;
-    location.href = 'http://127.0.0.1:5500/'
+    location.href = urlLogin
     localStorage.setItem('status',JSON.stringify({status:'none'}));
     localstorage.removeItem('user')
     ;
